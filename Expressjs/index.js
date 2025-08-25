289c261ede151e7710bee72d4879b4abce3ad661
Durgaprasad-Developer <bdurga.24bcs10012@sst.scaler.com>
2025-08-25 17:31:35 +0530

    tried routing

--- Expressjs/index.js
+++ Expressjs/index.js
@@ -8,6 +8,26 @@ app.get('/', (req,res)=>{
 });
 app.get('/about', (req, res)=>{
     res.send("This is Durga prasad Reddy");
+});
+
+const courses = [
+    {courseid:1, name:'Java', instructor: 'Kshritij'},
+    {courseid:2, name:'JavaScript', instructor: 'Durga'},
+    {courseid:3, name:'DBMS', instructor: 'Reddy'},
+    {courseid:4, name:'Data Structures', instructor: 'Kshritij'},
+]
+//get all courses
+app.get('/courses', (req, res)=>{
+    console.log(req);
+    res.send(courses.params);
+})
+
+// to get a single course based on id
+app.get('/courses/:id', (req, res)=>{
+    const course = courses.find(c => c.courseid === parseInt(req.params.id));
+    if(!course) return res.status(404).send('The course with the given ID was not found');
+    console.log(req.params);
+    res.send(course);
 })
 
 // Read
