05842c52ac46f422bf0e29fba020e27b6dad2311
Durgaprasad-Developer <bdurga.24bcs10012@sst.scaler.com>
2025-08-26 22:19:03 +0530

    Applied Mongodb 1 used Mongoose, 2 Created a connection, 3 created courseSchema, 4 created CourseModel, 5 applied get, post operation

---
+++ MongoDB/Server/index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json())
+
+
+mongoose.connect(
+  "mongodb+srv://durgaprasad:durga@cluster0.yivezvw.mongodb.net/mydb?retryWrites=true&w=majority&appName=Cluster0"
+).then(() => {
+  console.log("DB connected");
+}).catch((err) => {
+  console.log(err);
+});
+
+// Course Schema
+
+let courseSchema = new mongoose.Schema({
+    course_name:{
+        type: String,
+        required: true
+    },
+
+    instructor:{
+        type: String,
+        required: true
+    },
+
+    ratings:{
+        type:Number,
+    },
+    isLive:{
+        type:Boolean,
+        required:true
+    }
+})
+
+const CourseModel = mongoose.model('courses',courseSchema);
+
+
+app.get("/", (req, res) => {
+  res.send("Message From Server");
+});
+app.get("/courses", async(req, res)=>{
+    try{
+    const courses = await CourseModel.find();
+    res.status(200).send(courses);
+    }catch(err){
+        res.status(500).send(err.message)
+    }
+})
+
+app.get("/courses/:_id", async(req,res)=>{
+    try{
+        const course = await CourseModel.findById(req.params._id)
+        res.status(200).send(course);
+    }catch(err){
+        res.status(404).send(err.message);
+    }
+})
+
+app.post('/courses', async(req, res)=>{
+    try{
+        let course = await CourseModel.create({
+        course_name: req.body.course_name,
+        instructor:req.body.instructor,
+        ratings:req.body.ratings,
+        isLive:req.body.isLive
+    })
+    res.status(201).send(course);
+    }catch(err){
+        res.status(400).send(err.message)
+    }
+    
+})
+
+app.listen(8801, () => {
+  console.log("Server Started");
+});
